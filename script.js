80213ad2950f69942e40accd1c4d9fa1e826297d
kylemarx86 <kylemarx86@csu.fullerton.edu>
2017-03-05 17:10:33 -0800

    dynamic addig of svg elements to cards. all cards treated as same. only one suit.

--- script.js
+++ script.js
@@ -1,3 +1,42 @@
 $(document).ready(function(){
     console.log('ready');
+    draw_cards();
 });
+
+function draw_cards(){
+    console.log('draw_cards');
+    add_numbers_to_card('6');
+    add_suit_to_card();
+}
+
+function draw_card(){
+
+}
+
+function add_numbers_to_card(number){
+    $('.card_number_top').text(number);
+    $('.card_number_bottom').text(number);
+}
+
+//add a parameter to this once if drawn out all suits
+function add_suit_to_card(){
+    // create div with svg within
+    var $div = $('<div>');
+    $div.html('<svg xmlns="http://www.w3.org/2000/svg"></svg>');
+    $svg = $div.find('svg');
+    $svg.attr({'width':'49', 'height':'49'});
+    // create group
+    var group = document.createElementNS("http://www.w3.org/2000/svg","g");
+    // create rect with background fill
+            //change second part of attribute once i have all suits drawn out
+    var $rect = document.createElementNS("http://www.w3.org/2000/svg","rect");
+    $rect.setAttribute('style', 'fill: url("#clubs"); width:49; height:49;');
+    // add rect to group
+    group.appendChild($rect);
+    // add group to svg
+    $svg.append(group);
+    // add div to card
+    $('.token_area').append($div);
+}
+
+var rank_classes = ['rankA', 'rank2', 'rank3', 'rank4', 'rank5', 'rank6', 'rank7', 'rank8', 'rank9', 'rank10', 'rankJ', 'rankQ', 'rankK'];
