2087b21ea3cdd63cf67ec0a253b5af656bc12bef
kylemarx86 <kylemarx86@csu.fullerton.edu>
2017-03-06 13:48:32 -0800

    added parameter add_suit_to_card method.

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
+var rank__classes = ['rank_A', 'rank_2', 'rank_3', 'rank_4', 'rank_5', 'rank_6', 'rank_7', 'rank_8', 'rank_9', 'rank_10', 'rank_J', 'rank_Q', 'rank_K'];
+
 $(document).ready(function(){
-    console.log('ready');
     draw_cards();
 });
 
 function draw_cards(){
-    console.log('draw_cards');
     add_numbers_to_card('6');
-    add_suit_to_card('.suit');
+    add_suit_to_card('clubs');
 }
 
 function draw_card(){
@@ -19,12 +19,8 @@ function add_numbers_to_card(number){
 }
 
 //add a parameter to this once if drawn out all suits
-function add_suit_to_card(location_identifier){
-    // identify div to make svg in
-    // var $div = $('.token_area');
-    // var $div = $('.card_suit_top');
-    var $div = $(location_identifier);
-    
+function add_suit_to_card(suit){
+    var $div = $('.suit');
     
     //make svg
     $div.html('<svg xmlns="http://www.w3.org/2000/svg"></svg>');
@@ -33,9 +29,9 @@ function add_suit_to_card(location_identifier){
     // create group
     var group = document.createElementNS("http://www.w3.org/2000/svg","g");
     // create rect with background fill
-            //change second part of attribute once i have all suits drawn out
     var $rect = document.createElementNS("http://www.w3.org/2000/svg","rect");
-    $rect.setAttribute('style','fill: url("#clubs")');
+    //add suit to rect
+    $rect.setAttribute('style','fill: url("#' + suit + '")');
     $rect.setAttribute('width', '49');
     $rect.setAttribute('height', '49');
     $rect.setAttribute('transform', 'scale(0.5)');
@@ -44,5 +40,3 @@ function add_suit_to_card(location_identifier){
     // add group to svg
     $svg.append(group);
 }
-
-var rank_classes = ['rankA', 'rank2', 'rank3', 'rank4', 'rank5', 'rank6', 'rank7', 'rank8', 'rank9', 'rank10', 'rankJ', 'rankQ', 'rankK'];
