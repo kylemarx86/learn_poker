bdf8e3ec4ac56d33874a7120e5ce10eefc048d50
kylemarx86 <kylemarx86@csu.fullerton.edu>
2017-03-28 22:59:30 -0700

    new attempt at node run

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 var app = express();
 
+app.use(express.static('home.html'));
 
 // app.get('/', (req, res) => {
 //     res.send('home.html')
@@ -15,9 +16,9 @@ var app = express();
 //     res.render('home.hbs');
 // });
 
-app.get('/', (req, res) => {
-    res.render('home.hbs');
-});
+// app.get('/', (req, res) => {
+//     res.render('home.hbs');
+// });
 
 app.listen(3000, () => {
     console.log(`server is up on port 3000`);
