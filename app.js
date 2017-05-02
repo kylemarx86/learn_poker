0948503d9d83a2c252de52a41bd57daa0ee5837c
kylemarx86 <kylemarx86@csu.fullerton.edu>
2017-05-01 22:27:36 -0700

    change static port to dynamic.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 var app = express();
+const port = process.env.PORT || 3000;
 
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
@@ -9,6 +10,6 @@ app.get('/', (req, res) => {
     res.render('home.hbs');
 });
 
-app.listen(3000, () => {
-    console.log(`server is up on port 3000`);
+app.listen(port, () => {
+    console.log(`server is up on port: ${port}`);
 });
