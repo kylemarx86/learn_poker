32aed0feb145c010957db27839af2a5dbe75cd51
kylemarx86 <kylemarx86@csu.fullerton.edu>
2017-11-15 14:10:03 -0800

    clean up unused tests.

--- public/script/utils/card.test.js
+++ public/script/utils/card.test.js
@@ -1,4 +1,3 @@
-
 const expect = require('expect');
 const rewire = require('rewire');
 require("amd-loader");
@@ -11,6 +10,9 @@ var card = rewire('./card');
 // // const hands = require('./public/script/player_hand');
 
 describe('card', () => {
+    var ind_card = {
+
+    }
     it('should create new card', () => {
         var new_card = new card(0, 1);
         expect(new_card).toExist();
@@ -18,36 +20,8 @@ describe('card', () => {
         //     throw new Error(`card doesn't exist.`);
         // }
     });
-    it('should convert number to rank', () => {
-        var new_card = new card(0, 1);
-        // expect(card.card.convert_number_to_rank()).toBe('2');
-    });
+    // it('should convert number to rank', () => {
+    //     var new_card = new card(0, 1);
+    //     // expect(card.card.convert_number_to_rank()).toBe('2');
+    // });
 });
-
-
-
-
-// app.__set__
-// app.__get__
-
-// describe('card', () =>{
-//     var db = {
-//         saveUser: expect.createSpy()
-//     };
-//     app.__set__('db', db);
-
-//     it('should convert number to rank', () => {
-//         var spy = expect.createSpy();
-//         spy('kyle', 31);
-//         // expect(spy).toHaveBeenCalled();
-//         expect(spy).toHaveBeenCalledWith('kyle', 31);
-//     });
-
-//     it('should call saveUser with user object', () => {
-//         var email = 'ex@example.com';
-//         var password = '123abc';
-
-//         app.handleSignup(email, password);
-//         expect(db.saveUser).toHaveBeenCalledWith({email, password});
-//     });
-// });
