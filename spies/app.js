3ba63f2e873c314d31898f7dc70d16d8d56c2ea2
Kyle Marx <kylemarx86@csu.fullerton.edu>
2018-03-06 16:17:43 -0800

    remove unused resources. clean-up package.json

--- spies/app.js
+++
@@ -1,8 +0,0 @@
-var db = require('./db.js');
-
-module.exports.handleSignup = (email, password) => {
-    // check if email already exists
-    // save the user to the database
-    db.saveUser({email, password});
-    // send the welcome email
-}
--- spies/db.js
+++
@@ -1,3 +0,0 @@
-module.exports.saveUser = (user) => {
-    console.log('saving the user', user);
-}
