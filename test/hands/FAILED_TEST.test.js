277df9c6135bacc34eefc40e433cfc40a2d7ab28
Kyle Marx <kylemarx86@csu.fullerton.edu>
2020-10-12 06:05:42 -0700

    fixed wording in tests for failed_test.

--- test/hands/FAILED_TEST.test.js
+++ test/hands/FAILED_TEST.test.js
@@ -17,49 +17,49 @@ describe('FAILED TEST', () => {
 
     describe('expect NOT to find', () => {
         describe('Check straight flush', () => {
-            it(`should NOT find a ${hand_type} to be best hand`, () => {
+            it(`should NOT find a straight flush to be best hand`, () => {
                 var hand = new player_hand(hands.failed_test_hand_1);
                 expect(hand.display_best_hand()).toNotBe('straight flush').toBe(hand_type);
             });
         });
-        describe('Check four of a kind house', () => {
-            it(`should NOT find a ${hand_type} to be best hand`, () => {
+        describe('Check four of a kind', () => {
+            it(`should NOT find a four of a kind to be best hand`, () => {
                 var hand = new player_hand(hands.failed_test_hand_1);
                 expect(hand.display_best_hand()).toNotBe('four of a kind').toBe(hand_type);
             });
         });
         describe('Check flush', () => {
-            it(`should NOT find a ${hand_type} to be best hand`, () => {
+            it(`should NOT find a flush to be best hand`, () => {
                 var hand = new player_hand(hands.failed_test_hand_1);
                 expect(hand.display_best_hand()).toNotBe('flush').toBe(hand_type);
             });
         });
         describe('Check straight', () => {
-            it(`should NOT find a ${hand_type} to be best hand`, () => {
+            it(`should NOT find a straight to be best hand`, () => {
                 var hand = new player_hand(hands.failed_test_hand_1);
                 expect(hand.display_best_hand()).toNotBe('straight').toBe(hand_type);
             });
         });
         describe('Check three of a kind', () => {
-            it(`should NOT find a ${hand_type} to be best hand`, () => {
+            it(`should NOT find a three of a kind to be best hand`, () => {
                 var hand = new player_hand(hands.failed_test_hand_1);
                 expect(hand.display_best_hand()).toNotBe('three of a kind').toBe(hand_type);
             });
         });
         describe('Check two pairs', () => {
-            it(`should NOT find a ${hand_type} to be best hand`, () => {
+            it(`should NOT find a two pairs to be best hand`, () => {
                 var hand = new player_hand(hands.failed_test_hand_1);
                 expect(hand.display_best_hand()).toNotBe('two pairs').toBe(hand_type);
             });
         });
         describe('Check pair', () => {
-            it(`should NOT find a ${hand_type} to be best hand`, () => {
+            it(`should NOT find a pair to be best hand`, () => {
                 var hand = new player_hand(hands.failed_test_hand_1);
                 expect(hand.display_best_hand()).toNotBe('pair').toBe(hand_type);
             });
         });
         describe('Check high card', () => {
-            it(`should NOT find a ${hand_type} to be best hand`, () => {
+            it(`should NOT find a high card to be best hand`, () => {
                 var hand = new player_hand(hands.failed_test_hand_1);
                 expect(hand.display_best_hand()).toNotBe('high card').toBe(hand_type);
             });
